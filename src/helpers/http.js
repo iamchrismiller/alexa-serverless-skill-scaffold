82516a818964370da7593e9146b53412a748e550
Chris Miller <chrismillersd@gmail.com>
2016-12-29 16:09:34 -0800

    Initial SSO Translation Lambda, WIP

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -6,3 +6,14 @@ module.exports.defaultHeaders = {
   // Lets figure out how to lock this down
   'Access-Control-Allow-Origin': '*',
 };
+
+/**
+ * Convert Object to URL Encoded Query String
+ * @param {object} obj
+ * @return {string}
+ */
+module.exports.objectToQueryString = function(obj) {
+  return Object.keys(obj).map((key) => {
+    return (`${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`).join('&');
+  });
+};
--- src/lambdas/authorize.js
+++ src/lambdas/authorize.js
@@ -1,6 +1,58 @@
+// const https = require('https');
 
 const policies = require('./../helpers/policies');
-const auth = require('./../helpers/auth');
+const authHelper = require('./../helpers/auth');
+const httpHelper = require('./../helpers/http');
+
+/**
+ * Translate SSO Response into Alexa Compatible Keys
+ * @NOTE This may be a candidate for a seperate project
+ * @see https://goo.gl/9UY1rh
+ * @param {object} event
+ * @param {object} context
+ * @param {function} callback
+ */
+module.exports.translate = (event, context, callback) => {
+  console.log('Attempting To Translate SSO Response', event, context);
+
+  const requestOptions = {
+    // This may want to be dynamic
+    host: 'https://layla.amazon.com',
+    path: '/spa/skill/account-linking-status.html?' +
+      httpHelper.objectToQueryString({
+        state: {},
+        // Not sure how this is generated yet?
+        vendorId: 'M1DVLGCUZEYLAC',
+        client_id: 'dummy-client',
+        access_token: 'bogus-token',
+        token_type: 'Bearer',
+      }),
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  console.log('requestOptions', requestOptions);
+
+  // const request = https.request(requestOptions, function(response) {
+  //   response.on('data', function (chunk) {
+  //     body += chunk;
+  //   });
+  //
+  //   callback(null, {
+  //     statusCode: 200,
+  //     headers: httpHelper.defaultHeaders,
+  //     body: JSON.stringify({
+  //       message: body,
+  //       input: event,
+  //     }),
+  //   });
+  // });
+  //
+  // request.write();
+  // request.end();
+};
 
 /**
  * Authorize Handler
@@ -10,7 +62,7 @@ const auth = require('./../helpers/auth');
  */
 module.exports.main = (event, context, callback) => {
   const principalId = 'user';
-  if (auth.isTokenValid(event.accessToken)) {
+  if (authHelper.isTokenValid(event.accessToken)) {
     callback(null, policies.generate(principalId, 'Allow', event.methodArn));
     return;
   }
